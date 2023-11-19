d253bab0bbcfaa9b879b9a1d87b2398936f107e9
Diego Maia Fernandes <93396521+diegomaia04@users.noreply.github.com>
2023-11-19 14:39:40 -0300

    Add files via upload

---
+++ Exercicios Python/ListaSequencial/Ex3.py
@@ -0,0 +1,10 @@
+##exercicio
+##faca um programa que peça dois nnuemros e imprima a soma
+
+
+numero1 = float(input("informe um numero de sua escolha: \n"))
+numero2 = float(input("informe um nuemro igual ou diferenet, de sua escolha: \n"))
+
+soma = numero1 + numero2
+
+print("a soma deles é: ", + soma)
