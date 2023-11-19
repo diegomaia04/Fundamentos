807f8091e6b83b4a636881193c614fbf03bfd6ee
Diego Maia Fernandes <93396521+diegomaia04@users.noreply.github.com>
2023-11-19 16:11:09 -0300

    Add files via upload

---
+++ Exercicios Python/ListaDecisao/Ex1.py
@@ -0,0 +1,10 @@
+## exercico 01
+## faca um programa que receba dois nuemros e exib o maior entre eles
+
+num1 = int(input("Digite um nuemro inteiro: "))
+num2 = int(input("Digite um nuemro inteiro: "))
+
+if num1 > num2:
+    print(" o maiior entre os numeros é o:", num1)
+else:
+    print(" o maiior entre os numeros é o:", num2)
---
+++ Exercicios Python/ListaDecisao/Ex6.py
@@ -0,0 +1,1 @@
+# exercio 6
